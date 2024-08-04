e41c0df50f1b0de0ada2039ab07f7a3d01e5501a
James Tanner <tanner.jc@gmail.com>
2024-08-04 13:55:15 -0400

    Add ALL of the models to rbac.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/models/__init__.py
+++ galaxy_ng/app/models/__init__.py
@@ -13,6 +13,19 @@ from .namespace import Namespace, NamespaceLink
 from .organization import Organization, Team
 from .synclist import SyncList
 
+from pulp_ansible.app.models import (
+    AnsibleCollectionDeprecated,
+    AnsibleDistribution,
+    AnsibleNamespace,
+    AnsibleNamespaceMetadata,
+    AnsibleRepository,
+    Collection,
+    CollectionRemote,
+    CollectionVersion,
+    CollectionVersionSignature,
+    Tag,
+)
+
 from ansible_base.rbac import permission_registry
 
 __all__ = (
@@ -41,9 +54,57 @@ __all__ = (
     "SyncList",
 )
 
+permission_registry.register(
+    AnsibleCollectionDeprecated, parent_field_name=None
+)
+permission_registry.register(
+    AnsibleDistribution, parent_field_name=None
+)
+permission_registry.register(
+    AnsibleNamespace, parent_field_name=None
+)
+permission_registry.register(
+    AnsibleNamespaceMetadata, parent_field_name=None
+)
+permission_registry.register(
+    AnsibleRepository, parent_field_name=None
+)
+permission_registry.register(
+    Collection, parent_field_name=None
+)
+permission_registry.register(
+    CollectionRemote, parent_field_name=None
+)
+permission_registry.register(
+    CollectionVersion, parent_field_name=None
+)
+permission_registry.register(
+    CollectionVersionSignature, parent_field_name=None
+)
+permission_registry.register(
+    Tag, parent_field_name=None
+)
 
 permission_registry.register(
     Namespace, parent_field_name=None
+)
+permission_registry.register(
+    NamespaceLink, parent_field_name=None
+)
+permission_registry.register(
+    ContainerDistribution, parent_field_name=None
+)
+permission_registry.register(
+    ContainerDistroReadme, parent_field_name=None
+)
+permission_registry.register(
+    ContainerNamespace, parent_field_name=None
+)
+permission_registry.register(
+    ContainerRegistryRemote, parent_field_name=None
+)
+permission_registry.register(
+    ContainerRegistryRepos, parent_field_name=None
 )
 permission_registry.register(
     CollectionImport, parent_field_name='namespace'
