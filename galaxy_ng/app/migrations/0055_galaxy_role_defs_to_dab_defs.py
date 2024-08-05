90e2208b2dd602c9b0b45b4bfcbb64afe440cbb6
James Tanner <tanner.jc@gmail.com>
2024-08-05 13:48:55 -0400

    Fill in missing perms and semi-disable the roledef migration.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/migrations/0055_galaxy_role_defs_to_dab_defs.py
+++ galaxy_ng/app/migrations/0055_galaxy_role_defs_to_dab_defs.py
@@ -11,12 +11,29 @@ logger = logging.getLogger(__name__)
 
 def create_permissions_as_operation(apps, schema_editor):
     create_dab_permissions(global_apps.get_app_config("galaxy"), apps=apps)
+    Permission = apps.get_model('auth', 'Permission')
+    DABPermission = apps.get_model('dab_rbac', 'DABPermission')
+    for perm in Permission.objects.all():
+        print(f'CREATE {perm}')
+        dab_perm, created = DABPermission.objects.get_or_create(
+            codename=perm.codename,
+            content_type=perm.content_type,
+            name=perm.name
+        )
+
+
+def reverse_create_permissions_as_operation(apps, schema_editor):
+    Permission = apps.get_model('dab_rbac', 'DABPermission')
+    for perm in Permission.objects.all():
+        print(f'DELETE {perm}')
+        perm.delete()
 
 
 def copy_permissions_to_role_definitions(apps, schema_editor):
     Permission = apps.get_model('dab_rbac', 'DABPermission')
     RoleDefinition = apps.get_model('dab_rbac', 'RoleDefinition')
 
+    '''
     # TODO: migrate from pulp role model or something like that
     permissions = Permission.objects.all().filter(name__icontains='namespace')
     rd, created = RoleDefinition.objects.get_or_create(
@@ -30,6 +47,14 @@ def copy_permissions_to_role_definitions(apps, schema_editor):
         logger.info(f'Created RoleDefinition {rd.name}')
         for perm in permissions:
             rd.permissions.add(perm)
+    '''
+
+
+def reverse_copy_permissions_to_role_definitions(apps, schema_editor):
+    RoleDefinition = apps.get_model('dab_rbac', 'RoleDefinition')
+    for roledef in RoleDefinition.objects.all():
+        print(f'DELTE {roledef}')
+        roledef.delete()
 
 
 class Migration(migrations.Migration):
@@ -40,6 +65,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(create_permissions_as_operation, migrations.RunPython.noop),
-        migrations.RunPython(copy_permissions_to_role_definitions),
+        migrations.RunPython(
+            create_permissions_as_operation,
+            reverse_create_permissions_as_operation
+        ),
+        migrations.RunPython(
+            copy_permissions_to_role_definitions,
+            reverse_copy_permissions_to_role_definitions
+        ),
     ]
