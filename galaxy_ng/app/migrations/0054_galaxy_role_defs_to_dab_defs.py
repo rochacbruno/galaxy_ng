890d3d4ba8dbaa40b48acd4cc26c95aadcf90b3e
James Tanner <tanner.jc@gmail.com>
2024-08-06 10:12:40 -0400

    Incomplete migration.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- galaxy_ng/app/migrations/0054_galaxy_role_defs_to_dab_defs.py
+++ galaxy_ng/app/migrations/0054_galaxy_role_defs_to_dab_defs.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from django.db import migrations
 
@@ -14,52 +15,72 @@ def create_permissions_as_operation(apps, schema_editor):
     Permission = apps.get_model('auth', 'Permission')
     DABPermission = apps.get_model('dab_rbac', 'DABPermission')
     for perm in Permission.objects.all():
-        print(f'CREATE {perm}')
+        print(f'CREATE {perm} {perm.codename}')
         dab_perm, created = DABPermission.objects.get_or_create(
             codename=perm.codename,
             content_type=perm.content_type,
             name=perm.name
         )
 
+    print(f'FINISHED CREATING PERMISSIONS')
+
 
 def reverse_create_permissions_as_operation(apps, schema_editor):
     Permission = apps.get_model('dab_rbac', 'DABPermission')
     for perm in Permission.objects.all():
-        print(f'DELETE {perm}')
+        print(f'DELETE {perm} {perm.codename}')
         perm.delete()
 
 
-def copy_permissions_to_role_definitions(apps, schema_editor):
-    Permission = apps.get_model('dab_rbac', 'DABPermission')
+def copy_roles_to_role_definitions(apps, schema_editor):
+    Role = apps.get_model('core', 'Role')
+    DABPermission = apps.get_model('dab_rbac', 'DABPermission')
     RoleDefinition = apps.get_model('dab_rbac', 'RoleDefinition')
 
-    '''
-    # TODO: migrate from pulp role model or something like that
-    permissions = Permission.objects.all().filter(name__icontains='namespace')
-    rd, created = RoleDefinition.objects.get_or_create(
-        name='Namespace Admin',
-        defaults=dict(
-            content_type=permissions[0].content_type,
-            managed=True
-        )
-    )
-    if created:
-        logger.info(f'Created RoleDefinition {rd.name}')
-        for perm in permissions:
-            rd.permissions.add(perm)
-    '''
+    while True:
+        print('-' * 100)
+        print(f'{Role.objects.count()}')
+        print('-' * 100)
+        break
+
+        if Role.objects.count() > 0:
+            break
+        time.sleep(2)
+
+    for corerole in Role.objects.all():
+        print(f'CREATE {corerole} {corerole.name}')
+        roledef, _ = RoleDefinition.objects.get_or_create(name=corerole.name)
+
+        content_types = set()
+        for perm in corerole.permissions.all():
+            dabperm = DABPermission.objects.get(
+                codename=perm.codename,
+                content_type=perm.content_type,
+                name=perm.name
+            )
+            roledef.permissions.add(dabperm)
+            content_types.add(perm.content_type)
+
+        # FIXME - when dab supports multi-content type roles
+        content_types = list(content_types)
+        if len(content_types) == 1:
+            roledef.content_type = content_types[0]
+            roledef.content_type_id = content_types[0].id
+            roledef.save()
 
 
-def reverse_copy_permissions_to_role_definitions(apps, schema_editor):
+def reverse_copy_roles_to_role_definitions(apps, schema_editor):
     RoleDefinition = apps.get_model('dab_rbac', 'RoleDefinition')
     for roledef in RoleDefinition.objects.all():
-        print(f'DELTE {roledef}')
+        print(f'DELETE {roledef} {roledef.name}')
         roledef.delete()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('core', '0117_task_unblocked_at'),
+        ('ansible', '0055_alter_collectionversion_version_alter_role_version'),
         ('galaxy', '0053_wait_for_dab_rbac'),
         ('dab_rbac', '__first__'),
     ]
@@ -70,7 +91,7 @@ class Migration(migrations.Migration):
             reverse_create_permissions_as_operation
         ),
         migrations.RunPython(
-            copy_permissions_to_role_definitions,
-            reverse_copy_permissions_to_role_definitions
+            copy_roles_to_role_definitions,
+            reverse_copy_roles_to_role_definitions
         ),
     ]
